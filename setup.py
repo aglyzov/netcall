ce1abeb3f2f811fb9a6bac97705d4e95e3ab4159
Alexander Glyzov <bonoba@gmail.com>
2014-08-28 01:54:42 +0400

    bumped version to 0.4.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "netcall",
-    version = "0.4.0",
+    version = "0.4.1",
     packages = find_packages(),
 
     install_requires = ['pyzmq', 'pebble'],
